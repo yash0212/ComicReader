bbeeb6c7b164994ad33bb3a426a7590bce5b6577
Yash <yashlotan7@gmail.com>
2019-11-03 00:33:24 +0530

    Comic directory selection screen added

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const Store = require("electron-store");
+const store = new Store();
 
+const isMac = process.platform === "darwin";
 let win;
 
 function createWindow() {
-  let win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    show: false
   });
 
-  win.loadFile("index.html");
+  var comicDir = store.get("comicDir");
+  if (comicDir === undefined || comicDir === "") {
+    win.loadFile("index.html");
+  } else {
+    win.loadFile("comicdir.html");
+  }
+  //   win.webContents.openDevTools();
 
-  win.webContents.openDevTools();
-
+  win.on("ready-to-show", () => {
+    win.show();
+  });
   win.on("closed", function() {
     win = null;
   });
 }
+
+ipcMain.on("index", (e, data) => {
+  if (data.msg == "dirSet") {
+    win.loadFile("comicdir.html");
+  }
+});
 
 app.on("ready", createWindow);
 
